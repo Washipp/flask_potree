af9830868b00e87abad4f65289780a6de78d05c8
Silas Meier <silas.meier@bluewin.ch>
2022-06-23 14:20:04 +0200

    updated example.py

--- example.py
+++ example.py
@@ -7,14 +7,22 @@ from src.colmap_manager import construct_cameras
 app = Tarasp(port=5000)
 # app.load_example_elements()
 
-point_cloud = PotreePointCloud(data='data/fragment.ply', name="ETH-CAB", group="Group 1")
-app.add_element(point_cloud)
 
-ls = LineSet(name='Line Set', group=["Group 1", "Group 2", "Group 2", "Group 2"])
+ls = LineSet(data=[[(-10, -5, 0), (-10, 5, 0)]], name='Line Set Here', group=["Group 1", "Group 2", "Group 3"])
 app.add_element(ls)
 
-ct = CameraTrajectory(image_url='data/images/03903474_1471484089.jpg')
+
+ct = CameraTrajectory(data=([2, 2, 2], [2, 2, 2, 2]), image_url='data/images/03903474_1471484089.jpg')
 app.add_element(ct)
+app.add_element(LineSet(data=[[(-20, -20, 10), (10, 50, 0)]], name='Line Set', group="Group 1"))
+
+
+point_cloud = PotreePointCloud(data='data/fragment.ply', name="Open3D example", group="Group 1")
+app.add_element(point_cloud)
+
+# 8GB took 360.452s to convert
+pc = PotreePointCloud(data='../data/mesh.ply', name="ETH-CAB Mesh 8GB", group="Group 1")
+app.add_element(pc)
 
 # path = './data/colmap/demo/sfm'
 #
