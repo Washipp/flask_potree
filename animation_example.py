106c3504de7f12fa11d44c6f33acac12b4f35aef
Silas Meier <silas.meier@bluewin.ch>
2022-06-27 11:43:31 +0200

    Updated animation_example.py

--- animation_example.py
+++ animation_example.py
@@ -1,17 +1,16 @@
 from src.App.app import Tarasp
-from src.Components.base import Camera
+from src.Components.base import CameraState
 from src.SceneElements.elements import PotreePointCloud
-
-path = './data/colmap/demo/sfm'
 
 app = Tarasp(print_component_tree=True)
 
-app.add_element(PotreePointCloud(data='../data/mesh.ply', name="Fragment", group="Group 1"))
+app.add_element(PotreePointCloud(data='../data/mesh.ply'))
 
-animation_name = "animation_1"
 
-for i in range(50):
-    cam = Camera([100, 100 + i / 2, 100 + i], [0.5, 0.5, 0.5, 2, 0.5])
-    app.add_animation(animation_name, cam)
+def animation_function(index: int) -> CameraState:
+    return CameraState([50, 50 + (index % 50), 50 + (index % 50)], [0.5, 0.5, 0.5, 2, 0.5])
+
+
+app.add_animation("animation_1", animation_function)
 
 app.run()
